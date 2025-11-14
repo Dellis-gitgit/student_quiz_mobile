51f169653d0ae925c675561cb83b17c4b45dda72
John Dellis <dellisgiannis2018@gmail.com>
2025-11-14 06:18:28 +0200

    Updates

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function showResult() {
     resultDesc.innerText = resultDescriptions[most];
 
     //Change the image according to the result
-    const BASE = "student_quiz_mobile/images/";
+    const BASE = "student_quiz_mobile/public/images/";
 
     if (most === 'a') resultImg.src = BASE + "resultA.jpg";
     else if (most === 'b') resultImg.src = BASE + "resultB.jpg";
